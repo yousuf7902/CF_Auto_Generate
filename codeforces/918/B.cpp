82c46b3e27cce9b690c0caa5f9dbc280e5703ea3
Yousuf Hassan <errormr808@gmail.com>
2022-07-13 14:17:00 +0600

    Add solution for problem `B - Radio Station` Link: https://codeforces.com/contest/918/problem/B Tags: implementation, strings, *900 Ref: https://codeforces.com/contest/918/submission/164004744

---
+++ codeforces/918/B.cpp
@@ -0,0 +1,63 @@
+// Problem: B. Radio Station
+// Contest: Codeforces - Codeforces Round #459 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/918/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+
+//~"~"~"~"~"~"~"~"~"~"~"~"~  ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥   ~"~"~"~"~"~"~"~"~"~"~"~"~//  
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+// ##                                                                         ## //
+// ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
+// ##                                                                         ## //
+// ##                        CF Handle : Yousuf7902                           ## //
+// ##                                                                         ## //
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//===============================================================================
+//
+#define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define         vc vector
+#define         pair pair<int, int>
+#define         st set<int>
+#define         ll long long
+#define         eb emplace_back
+#define         prec(n) fixed<<setprecision(n)
+#define         py      cout << "Yes\n";
+#define         pn      cout << "No\n";
+#define			endl     "\n"
+#define         rn return 0;
+//
+//===============================================================================
+
+int main(){
+    FAST
+    
+    int n,m;
+    cin>>n>>m;
+    
+    map<string, string>mp1;
+    
+    for(int i=0;i<n;i++){
+        string s1,s2;
+        cin>>s1>>s2;
+        
+        mp1[s2]=s1;
+    }
+    
+    while(m--){
+        string s1,s2,ip;
+        cin>>s1>>s2;
+        ip=s2;
+        s2.pop_back();
+        
+        cout<<s1<<" "<<ip<<" #"<<mp1[s2]<<endl;
+        
+    }
+    
+    
+    rn;
+}
