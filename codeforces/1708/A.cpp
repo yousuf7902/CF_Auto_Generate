491f004ff9266da2bfd76d7433a8b918e1f79faa
Yousuf Hassan <errormr808@gmail.com>
2022-07-17 21:16:00 +0600

    Add solution for problem `A - Difference Operations` Link: https://codeforces.com/contest/1708/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1708/submission/164628519

---
+++ codeforces/1708/A.cpp
@@ -0,0 +1,72 @@
+// Problem: A. Difference Operations
+// Contest: Codeforces - Codeforces Round #808 (Div. 2)
+// URL: https://codeforces.com/contest/1708/problem/0
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+
+//~"~"~"~"~"~"~"~"~"~"~"~"~  ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥   ~"~"~"~"~"~"~"~"~"~"~"~"~//  
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+// ##                                                                         ## //
+// ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
+// ##                                                                         ## //
+// ##                        CF Handle : Yousuf7902                           ## //
+// ##                                                                         ## //
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//===============================================================================
+//
+#define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define         vc vector
+#define         pair pair<int, int>
+#define         st set<int>
+#define         ll long long
+#define         eb emplace_back
+#define         prec(n) fixed<<setprecision(n)
+#define         py      cout << "Yes\n";
+#define         pn      cout << "No\n";
+#define			endl     "\n"
+#define         rn return 0;
+//
+//===============================================================================
+
+int main(){
+    FAST
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        vc <int> vec;
+        
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            
+            vec.eb(x);
+        }
+
+        int f=0;
+        for(int i=vec.size()-1;i>=1;i--){
+            if(vec[i]%vec[0]!=0){
+                f=1;
+                break;
+            }
+        }
+        
+        if(f==1){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }    
+    
+    rn;
+}
