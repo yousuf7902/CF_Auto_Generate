13a6fee981ad777e5d1ee79294eb343690a44358
Yousuf Hassan <errormr808@gmail.com>
2022-08-22 21:56:00 +0600

    Add solution for problem `A - Crossmarket` Link: https://codeforces.com/contest/1715/problem/A Tags: constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1715/submission/169352474

---
+++ codeforces/1715/A.cpp
@@ -0,0 +1,57 @@
+// Problem: A. Crossmarket
+// Contest: Codeforces - Codeforces Round #816 (Div. 2)
+// URL: https://codeforces.com/contest/1715/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+
+//~"~"~"~"~"~"~"~"~"~"~"~"~  ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥   ~"~"~"~"~"~"~"~"~"~"~"~"~//  
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+// ##                                                                         ## //
+// ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
+// ##                                                                         ## //
+// ##                        CF Handle : Yousuf7902                           ## //
+// ##                                                                         ## //
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//===============================================================================
+//
+#define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define         vc vector
+#define         pair pair<int, int>
+#define         st set<int>
+#define         ll long long
+#define         eb emplace_back
+#define         prec(n) fixed<<setprecision(n)
+#define         py      cout << "Yes\n";
+#define         pn      cout << "No\n";
+#define			endl     "\n"
+#define         rn return 0;
+//
+//===============================================================================
+
+int main(){
+    FAST
+    
+    int t;
+    cin>>t;
+     
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        
+        if(n==1 && m==1){
+            cout<<0<<endl;
+        }
+        else{
+            int ans;
+            ans=min(n,m)+n+m-2;
+            cout<<ans<<endl;
+        }
+        
+    }
+    rn;
+}
