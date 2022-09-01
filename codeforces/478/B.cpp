88de2d6f00c280b72dcba33b228fc5c0c28c1070
Yousuf Hassan <errormr808@gmail.com>
2022-09-01 23:25:00 +0600

    Add solution for problem `B - Random Teams` Link: https://codeforces.com/contest/478/problem/B Tags: combinatorics, constructive algorithms, greedy, math, *1300 Ref: https://codeforces.com/contest/478/submission/170510427

---
+++ codeforces/478/B.cpp
@@ -0,0 +1,55 @@
+// Problem: B. Random Teams
+// Contest: Codeforces - Codeforces Round #273 (Div. 2)
+// URL: https://codeforces.com/contest/478/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+
+//~"~"~"~"~"~"~"~"~"~"~"~"~  ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥   ~"~"~"~"~"~"~"~"~"~"~"~"~//  
+ 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+// ##                                                                         ## //
+// ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
+// ##                                                                         ## //
+// ##                        CF Handle : Yousuf7902                           ## //
+// ##                                                                         ## //
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//===============================================================================
+//
+#define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define         vc vector
+#define         pair pair<int, int>
+#define         st set<int>
+#define         ll long long
+#define         eb emplace_back
+#define         prec(n) fixed<<setprecision(n)
+#define         py      cout << "Yes\n";
+#define         pn      cout << "No\n";
+#define			endl     "\n"
+#define         rn return 0;
+//
+//===============================================================================
+
+int main(){
+    FAST
+    
+    ll int n,m, maxi, mini;
+    
+    cin>>n>>m;
+    
+    ll int t,r;
+    
+    t=n/m;
+    r=n%m;
+    
+    maxi=((n-m+1)*(n-m))/2;
+    
+    mini=(t*(t-1)/2)*(m-r)+(t*(t+1)/2)*r;
+    
+    cout<<mini<<" "<<maxi<<endl;
+    
+    rn;
+}
