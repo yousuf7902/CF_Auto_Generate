39540df3824ef7d6e3e9b81fedeab4e38a9e65ce
Yousuf Hassan <mr.ahmedperfect80@gmail.com>
2022-09-12 12:27:00 +0600

    Add solution for problem `B - Lovely Palindromes` Link: https://codeforces.com/contest/688/problem/B Tags: constructive algorithms, math, *1000 Ref: https://codeforces.com/contest/688/submission/171779335

---
+++ codeforces/688/B.cpp
@@ -0,0 +1,42 @@
+//~"~"~"~"~"~"~"~"~"~"~"~"~  ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥   ~"~"~"~"~"~"~"~"~"~"~"~"~//
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+// ##                                                                         ## //
+// ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
+// ##                                                                         ## //
+// ##                        CF Handle : Yousuf7902                           ## //
+// ##                                                                         ## //
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//===============================================================================
+//
+#define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define         vc vector
+#define         pair pair<int, int>
+#define         st set<int>
+#define         ll long long
+#define         eb emplace_back
+#define         prec(n) fixed<<setprecision(n)
+#define         py      cout << "Yes\n";
+#define         pn      cout << "No\n";
+#define			endl     "\n"
+#define         rn return 0;
+//
+//===============================================================================
+
+int main(){
+    FAST
+
+    string s,s1;
+    cin>>s;
+
+    cout<<s;
+    for(int i=s.size()-1;i>=0;i--){
+        cout<<s[i];
+    }
+
+    rn;
+}
