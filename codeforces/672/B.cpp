0b75b707a07db54b02e95ead45940ba3ba08df42
Yousuf Hassan <mr.ahmedperfect80@gmail.com>
2022-09-12 11:57:00 +0600

    Add solution for problem `B - Different is Good` Link: https://codeforces.com/contest/672/problem/B Tags: constructive algorithms, implementation, strings, *1000 Ref: https://codeforces.com/contest/672/submission/171777459

---
+++ codeforces/672/B.cpp
@@ -0,0 +1,51 @@
+//~"~"~"~"~"~"~"~"~"~"~"~"~  ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥   ~"~"~"~"~"~"~"~"~"~"~"~"~//
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+// ##                                                                         ## //
+// ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
+// ##                                                                         ## //
+// ##                        CF Handle : Yousuf7902                           ## //
+// ##                                                                         ## //
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//===============================================================================
+//
+#define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define         vc vector
+#define         pair pair<int, int>
+#define         st set<int>
+#define         ll long long
+#define         eb emplace_back
+#define         prec(n) fixed<<setprecision(n)
+#define         py      cout << "Yes\n";
+#define         pn      cout << "No\n";
+#define			endl     "\n"
+#define         rn return 0;
+//
+//===============================================================================
+
+int main(){
+    FAST
+
+    int n;
+    cin>>n;
+
+    string s;
+    cin>>s;
+
+    if(n>26){
+        cout<<-1<<endl;
+    }
+    else{
+        set <char> dis;
+        for(int i=0;i<n;i++){
+            dis.insert(s[i]);
+        }
+        cout<<n-dis.size()<<endl;
+    }
+
+    rn;
+}
