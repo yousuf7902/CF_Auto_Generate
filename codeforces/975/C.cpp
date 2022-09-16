a28710ecacc8ecd711c61ce02c36778d932431ca
Yousuf Hassan <mr.ahmedperfect80@gmail.com>
2022-09-16 12:14:00 +0600

    Add solution for problem `C - Valhalla Siege` Link: https://codeforces.com/contest/975/problem/C Tags: binary search, *1400 Ref: https://codeforces.com/contest/975/submission/172316345

--- codeforces/975/C.cpp
+++ codeforces/975/C.cpp
@@ -1,53 +1,192 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-//===============================================================================
-//
-#define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
-#define         vc vector
-#define         pair pair<int, int>
-#define         st set<int>
-#define         ll long long
-#define         eb emplace_back
-#define         prec(n) fixed<<setprecision(n)
-#define         py      cout << "Yes\n";
-#define         pn      cout << "No\n";
-#define			endl     "\n"
-#define         rn return 0;
-//
-//===============================================================================
 
-int main(){
-    FAST
-    
-    int n,m;
-    cin>>n>>m;
-    
-    ll int s[n+1],k[m+1],pre[n+1];
-    
-    for(int i=1;i<=n;i++)cin>>s[i];
-    for(int i=1;i<=m;i++)cin>>k[i];
-    
-    pre[0]=0;
-    for(int i=1;i<=n;i++){
-        pre[i] = pre[i-1] + s[i];
+///              B I S M I L L A H I R  R A H M A N I R  R A H I M
+
+// ============================================================================ //
+// ||                                                                        || //
+// ||             International University of Business Agriculture           || //
+// ||                    and Technology, Dhaka, Bangladesh                   || //
+// ||                           Emrul Hasan Emon                             || //
+// ||                                                                        || //
+// ============================================================================ //
+
+#include<bits/stdc++.h>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+using namespace std;
+typedef bool                      boo;
+typedef int                       li;
+typedef long long int             ll;
+typedef unsigned long long int    l1;
+typedef double                    db;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+typedef vector < li >                   vli;
+typedef vector < ll >                   vll;
+typedef set < li >                      sli;
+typedef set < ll >                      sll;
+typedef pair < li, li >                pli;
+typedef pair < ll, li >                pll;
+typedef map < li,li >                  mli;
+typedef map < ll,ll >                  mll;
+typedef vector < pair < li, li > >     vpi;
+typedef vector < pair < ll, ll > >     vpl;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#define tc                        int t;cin>>t;while(t--)
+#define inp_i(a,n)                for(int ii = 0; ii < n ; ii++) cin >> a[ii]
+#define out_i(a, b, c)            for(int ii = b; ii < c; ii++) cout << a[ii] spc; cout nl;
+#define lp(i,a,b)                 for(i=a;i<b;i++)
+#define len(z)                    z.begin(),z.end()
+#define faster                    ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define input_txt                 freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define nl                        <<"\n"
+#define spc                       <<" "
+#define sp                        <<" "<<
+#define co(x)                     cout<<x nl
+#define sz(a)                     a.size()
+#define cy                        cout<<"YES" nl
+#define cn                        cout<<"NO" nl
+#define pb                        push_back
+#define F                         first
+#define S                         second
+#define pi                        2*acos(0.0)
+#define clr(z)                    z.clear()
+#define rn                        return
+#define gcd(a,b)                  __gcd(a,b)
+#define mem(b,z)                  memset(b,z,sizeof(b))
+#define fixed(x)                  fixed<<setprecision(x)
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const ll lx = 2e5 + 7;
+const ll mod = 998244353;
+
+ll a[lx], k[lx];
+ll pref[lx];
+
+void solve()
+{
+    int n, i, m;
+    cin >> n >> m;
+
+    for(i = 1; i <= n; i++) cin >> a[i];
+    for(i = 1; i <= m; i++) cin >> k[i];
+
+    for(i = 1; i <= n; i++)
+    {
+        pref[i] = pref[i-1] + a[i];
     }
-    
-    ll int sum=0,warr=0;
-    for(int i=1;i<=m;i++){
-        sum+=k[i];
-        
-        warr=upper_bound(pre,pre+n+1,sum)-pre;
-        warr--;
-        
-        if(warr==n){
-            cout<<n<<endl;
-            sum=0;
+    ll sum = 0;
+    for(i = 1; i <= m; i++)
+    {
+        sum += k[i];
+        int p = upper_bound(pref, pref+n+1, sum) - pref;
+        p--;
+
+        if(p == n)
+        {
+            cout << n << '\n';
+            sum = 0;
         }
-        else{
-            cout<<n-warr<<endl;
-        }
+        else cout << n-p << '\n';
     }
-    
-    rn;
 }
+
+void init_code()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+int main()
+{
+    faster
+
+    solve();
+
+// A L H A M D U L I L L A H
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 2 3 4 5....
+// Emrul Hasan Emon
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+
+ll f[lx];
+
+ll Big_Mod(ll a, ll b)
+{
+    if(b==0)
+        return 1;
+
+    ll ans;
+    ans=Big_Mod(a, b/2);
+    ans=(ans*ans)%mod;
+
+    if(b&1)
+        ans=(ans*a)%mod;
+
+    return ans;
+}
+
+void fact()
+{
+    ll ans=1;
+    f[0]=1;
+    for(ll i=1; i<lx; i++)
+    {
+        ans=(ans*i)%mod;
+        f[i]=ans;
+    }
+}
+
+ll nCr(ll n, ll k)
+{
+    return f[n]*1ll*Big_Mod(f[k], mod-2)%mod*1ll*Big_Mod(f[n-k], mod-2)%mod;
+}
+
+ll gcd(ll a, ll b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+void number_of_divisors(li n)
+{
+    //Number of divisors of every number between 1 to n
+    for(li i = 1; i*i <= n; i++)
+    {
+        for(li j = i*i; j <= n; j += i)
+        {
+            if(i*i == j)
+                a[j]++;
+            else
+                a[j] += 2;
+        }
+    }
+}
+
+void sum_of_divisor()
+{
+    for(li i = 1; i * i <= 1e7; i++)
+    {
+        for(li j = i*i; j < 1e7; j += i)
+        {
+            if(j == i*i)
+                a[j] += i;
+            else
+                a[j] += i+(j/i);
+        }
+    }
+}
+
+*/
+
+
+
