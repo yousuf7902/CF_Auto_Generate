fdbcb30799fc7c16f4b71a558feb30358b959381
Yousuf Hassan <mr.ahmedperfect80@gmail.com>
2022-09-16 12:15:00 +0600

    Add solution for problem `C - Valhalla Siege` Link: https://codeforces.com/contest/975/problem/C Tags: binary search, *1400 Ref: https://codeforces.com/contest/975/submission/172316440

---
+++ codeforces/975/C.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//===============================================================================
+//
+#define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define         vc vector
+#define         pair pair<int, int>
+#define         st set<int>
+#define         ll long long
+#define         eb emplace_back
+#define         prec(n) fixed<<setprecision(n)
+#define         py      cout << "Yes\n";
+#define         pn      cout << "No\n";
+#define			endl     "\n"
+#define         rn return 0;
+//
+//===============================================================================
+
+int main(){
+    FAST
+    
+    int n,m;
+    cin>>n>>m;
+    
+    ll int s[n+1],k[m+1],pre[n+1];
+    
+    for(int i=1;i<=n;i++)cin>>s[i];
+    for(int i=1;i<=m;i++)cin>>k[i];
+    
+    pre[0]=0;
+    for(int i=1;i<=n;i++){
+        pre[i] = pre[i-1] + s[i];
+    }
+    
+    ll int sum=0,warr=0;
+    for(int i=1;i<=m;i++){
+        sum+=k[i];
+        
+        warr=upper_bound(pre,pre+n+1,sum)-pre;
+        warr--;
+        
+        if(warr==n){
+            cout<<n<<endl;
+            sum=0;
+        }
+        else{
+            cout<<n-warr<<endl;
+        }
+    }
+    
+    rn;
+}
