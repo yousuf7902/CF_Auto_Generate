6a3cb203140452f789de2c8f9c3252ff1ae4ad88
Yousuf Hassan <errormr808@gmail.com>
2022-10-18 16:39:00 +0600

    Add solution for problem `B - Permutation Value` Link: https://codeforces.com/contest/1743/problem/B Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1743/submission/176864729

---
+++ codeforces/1743/B.cpp
@@ -0,0 +1,76 @@
+//~"~"~"~"~"~"~"~"~"~"~"~"~  ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥   ~"~"~"~"~"~"~"~"~"~"~"~"~//  
+ 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+// ##                                                                         ## //
+// ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
+// ##                                                                         ## //
+// ##                        CF Handle : Yousuf7902                           ## //
+// ##                                                                         ## //
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//===============================================================================
+//
+
+typedef bool                      boo;
+typedef long long int             ll;
+typedef unsigned long long int    l1;
+typedef double                    db;
+typedef vector<ll>                vc;
+typedef set<ll>                   st;
+typedef pair<ll, ll>              pr;
+typedef map<ll, ll>               mpll;
+typedef map<ll,char>              mpchar;
+typedef vector<pair<ll,ll>>       pr_vc;
+
+
+//===============================================================================
+//
+
+#define                  FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define test             int t;cin>>t;while(t--)
+#define eb               emplace_back
+#define prec(n)          fixed<<setprecision(n)
+#define py               cout << "Yes\n";
+#define pn               cout << "No\n";
+#define pY               cout << "YES\n";
+#define pN               cout << "NO\n";
+#define endl             "\n"
+#define F                first
+#define S                second
+#define gcd(a,b)         __gcd(a,b);
+#define rn               return 0;
+
+//
+//===============================================================================
+
+
+
+void solve(){
+    
+    int n;
+    cin>>n;
+    
+    int arr[n+1];
+    
+    for(int i=2;i<=n;i++){
+        cout<<i<<" ";
+    }
+    
+    cout<<'1'<<endl;
+    
+}
+
+
+int main(){
+    FAST
+    
+    test
+        
+    solve();
+
+    rn;
+}
