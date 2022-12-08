ddc03eb9a92070140833cc2d0308fbc3e5c3d92c
Yousuf Hassan <errormr808@gmail.com>
2022-12-08 20:15:00 +0600

    Add solution for problem `B - Ilya and Queries` Link: https://codeforces.com/contest/313/problem/B Tags: dp, implementation, *1100 Ref: https://codeforces.com/contest/313/submission/184340310

---
+++ codeforces/313/B.cpp
@@ -0,0 +1,91 @@
+// Problem: B. Ilya and Queries
+// Contest: Codeforces - Codeforces Round #186 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/313/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+
+//~"~"~"~"~"~"~"~"~"~"~"~"~  ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥   ~"~"~"~"~"~"~"~"~"~"~"~"~//  
+ 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+// ##                                                                         ## //
+// ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
+// ##                                                                         ## //
+// ##                        CF Handle : Yousuf7902                           ## //
+// ##                                                                         ## //
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//===============================================================================
+//
+
+typedef bool                              boo;
+typedef long long int                     ll;
+typedef unsigned long long int            l1;
+typedef double                            db;
+typedef vector<ll>                        vc;
+typedef set<ll>                           st;
+typedef pair<ll, ll>                      pr;
+typedef map<ll, ll>                       mpll;
+typedef map<ll,char>                      mpchar;
+typedef vector<pair<ll,ll>>               pr_vc;
+
+
+//===============================================================================
+//
+
+#define                        FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define test                   int t;cin>>t;while(t--)
+#define eb                     emplace_back
+#define prec(n)                fixed<<setprecision(n)
+#define py                     cout << "Yes\n";
+#define pn                     cout << "No\n";
+#define pY                     cout << "YES\n";
+#define pN                     cout << "NO\n";
+#define endl                   "\n"
+#define F                      first
+#define S                      second
+#define gcd(a,b)               __gcd(a,b);
+#define rn                     return 0;
+
+//
+//===============================================================================
+
+
+string s;
+
+
+int arr[100009];
+long long sum=0;
+
+int main(){
+    
+        FAST
+    
+        //test
+        
+        int n;
+        cin>>s>>n;
+        
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i]==s[i+1]){
+                sum++;
+                arr[i+1]=sum;
+            }
+            else{
+                arr[i+1]=sum;
+            }
+       }
+    
+        while(n--){
+            int a, b;
+            cin>>a>>b;
+            
+            cout<<arr[b-1]-arr[a-1]<<endl;
+              
+        }    
+
+        rn;
+}
